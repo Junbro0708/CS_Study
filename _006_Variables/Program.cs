8b85cb127583e251c88d371566f7016774eb992f
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-13 20:29:31 +0900

    C#

---
+++ _006_Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*------------------
+ * Name: _006_Variables
+ * DESC: 변수 선언의 기int num = 1000;
+            Console.WriteLine(num);
+------------------- */
+
+namespace _006_Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num = 1000;
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ _007_Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*------------------
+ * Name: _007_Variables
+ * DESC: 변수 선언의 다양한 방법
+------------------- */
+
+namespace _007_Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 200;
+            int a = 1000, b = 2000; // 변수 선언부
+
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+            Console.WriteLine("a : {0}", a);
+            Console.WriteLine("y : {0}", b);
+
+
+        }
+    }
+}
