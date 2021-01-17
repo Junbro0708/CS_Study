214c16ab351ccf01b4433211d569f9372c8e07b7
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-17 17:44:38 +0900

    C#

---
+++ _022_Data_var/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*------------------
+ * Name: _022_var
+ * DESC: var 데이터형 활용
+------------------- */
+
+namespace _022_Data_var
+{
+    class MainClass
+    {
+        // 지역 변수에만 사용이 가능하기 때문에 여기서 사용 불가
+
+        public static void Main(string[] args)
+        {
+            var a = 100; // 선언과 동시에 초기화
+
+            Console.WriteLine("a : {0}", a.GetType());
+        }
+    }
+}
---
+++ _023_Data_const/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*------------------
+ * Name: _023_const
+ * DESC: const 데이터형 활용
+------------------- */
+
+namespace _023_Data_const
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int MAX = 100;
+            const int MIN = 0;
+
+            Console.WriteLine("MIN : {0} ~ MAX : {1}", MIN, MAX);
+        }
+    }
+}
