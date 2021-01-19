f7ae1e44b17f330aa21e4f3e79502bcb3fd124c0
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-19 21:56:59 +0900

    C#

---
+++ _028_Data_Reference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*------------------
+ * Name: _028_Reference
+ * DESC: Reference의 기초
+------------------- */
+
+namespace _028_Data_Reference
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num = 100;
+            int refNum = num;
+
+            Console.WriteLine("{0} {1}", num == refNum, Object.ReferenceEquals(num, refNum));
+
+            string str = "HI";
+            string refStr = str;
+
+            Console.WriteLine("{0} {1}", str == refStr, Object.ReferenceEquals(str, refStr));
+        }
+    }
+}
