a1c86dda44e93567deb3b96b65b81568caacf9b2
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-28 21:02:16 +0900

    C#

---
+++ _058_Func/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _058_Func
+{
+    class MainClass
+    {
+        // 함수의 형태1 리턴형(x) 파라미터(x)
+
+        static void InitTitle() // main 함수가 static으로 되어있기 때문에 static을 써줘야 하는걸로 일단 이해
+        {
+            Console.WriteLine("짝수 홀수 보여주기");
+        }
+
+        static void PrintEven()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine("Even : {0}", i);
+                if (i % 2 != 0)
+                    Console.WriteLine("Odd : {0}", i);
+            }
+        }
+
+        // 함수의 형태2 리턴형(o) 파라미터(x)
+
+        static int Add()
+        {
+            int a = 0;
+            return a++;
+        }
+
+        // 함수의 형태3 리턴형(o) 파라미터(o)
+
+        static int Square(int i)
+        {
+            return i * i;
+        }
+
+        // 함수의 형태4 리턴형(x) 파라미터(o)
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Add());
+            Console.WriteLine(Square(5));
+        }
+    }
+}
---
+++ _062_Func_Swap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _062_Func_Swap
+{
+    class MainClass
+    {
+        static public void Swap(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("ValueSwap");
+            Console.WriteLine("num1 : {0} num2 : {1}", a, b);
+        }
+
+        static public void RefSwap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("RefSwap");
+            Console.WriteLine("num1 : {0} num2 : {1}", a, b);
+        }
+        public static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 5;
+
+            Swap(num1, num2);
+            Console.WriteLine("num1 : {0} num2 : {1}", num1, num2);
+
+            RefSwap(ref num1, ref num2);
+            Console.WriteLine("num1 : {0} num2 : {1}", num1, num2);
+        }
+    }
+}
