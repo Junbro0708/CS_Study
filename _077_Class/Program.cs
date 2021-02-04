18176853114e5e9b34faed0409693dd753547ad1
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-02-04 15:44:18 +0900

    C#

---
+++ _077_Class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _077_Class
+{
+    class AA
+    {
+        //멤버 변수
+        int num1; //기본적으로 private 속성
+        public int num2, num3;
+
+        //멤버 함수
+        public void Print()
+        {
+            Console.WriteLine("{0}, {1}, {2}", num1, num2, num3);
+        }
+        private void PrintPrivate()
+        {
+            Console.WriteLine("{0}, {1}, {2}", num1, num2, num3);
+        }
+    }
+    class MainClass
+    {
+        
+        public static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.num2 = 10;
+            aa.num3 = 100;
+            aa.Print();
+        }
+    }
+}
---
+++ _078_Class_Constructor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _078_Class_Constructor
+{
+    class AA
+    {
+        int a;
+        float f;
+
+        public AA()
+        {
+            a = 0;
+            f = 0f;
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine("a : {0} ", a);
+            Console.WriteLine("f : {0} ", f);
+        }
+        public AA(int _a, float _f = 0f)
+        {
+            a = _a;
+            f = _f;
+            Console.WriteLine(" Call Constructor ");
+            Console.WriteLine("a : {0} ", a);
+            Console.WriteLine("f : {0} ", f);
+        }
+        ~AA()
+        {
+            Console.WriteLine(" Call Destructor ");
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            AA aa = new AA();
+            AA aaa = new AA(3, 4f);
+        }
+    }
+}
