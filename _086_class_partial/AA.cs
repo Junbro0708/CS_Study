0b2ab7d8ae2b474be3b71b439f2a431eeda28dd5
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-02-18 21:39:59 +0900

    C#

---
+++ _086_class_partial/AA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace _086_class_partial
+{
+    public class AA
+    {
+        public AA()
+        {
+        }
+    }
+}
---
+++ _086_class_partial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _086_class_partial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
