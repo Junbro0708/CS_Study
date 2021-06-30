ce19b1a98e9f724528e185cacf62241334af463a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-06-30 23:46:24 +0900

    C# Studying

---
+++ _103_GenericFunc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _103_GenericFunc
+{
+    class MainClass
+    {
+        static void GenericPrint<T>(T data)
+        {
+            Console.WriteLine("data : {0}", data);
+        }
+
+        static void GenericPrint<T>(T[] arrData)
+        {
+            for(int i = 0; i < arrData.Length; i++)
+            {
+                Console.WriteLine("data : {0}", arrData[i]);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            float b = 10.3f;
+            string c = "Hello";
+
+            int[] arrA = { 1, 3, 4, 5, 6 };
+
+            GenericPrint(a);
+            GenericPrint(b);
+            GenericPrint(c);
+
+            GenericPrint(arrA);
+        }
+    }
+}
---
+++ _104_GenericClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _104_GenericClass
+{
+    class GenericAA<T>
+    {
+        private T num;
+
+        public T GetNum()
+        {
+            return num;
+        }
+
+        public void SetNum(T data)
+        {
+            num = data;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            GenericAA<int> AA = new GenericAA<int>();
+            AA.SetNum(100);
+            Console.WriteLine("AA : " + AA.GetNum());
+
+            GenericAA<float> BB = new GenericAA<float>();
+            BB.SetNum(100.3f);
+            Console.WriteLine("BB : " + BB.GetNum());
+        }
+    }
+}
